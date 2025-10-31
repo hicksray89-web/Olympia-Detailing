ea77dbe44e4615ae72c7fea8c86da008c31dd6b5
hicksray89-web <hicksray89@gmail.com>
2025-10-31 16:32:20 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{fontFamily: 'sans-serif', padding: '2rem'}}>
-      <h1>Olympia Automotive Detailing</h1>
-      <p>Premium auto detailing services in Olympia, WA.</p>
-      <a href="https://clienthub.getjobber.com/booking/b281cc79-fb56-4702-b259-c5aa69c37e0b">Book Now</a>
-    </main>
-  );
-}
